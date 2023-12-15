9ff26d4dd4528b3b110168ba6e1f08401cb69feb
oazula <oazula@unal.edu.co>
2023-12-14 22:05:45 -0500

    con-codium

--- dlc.py
+++ dlc.py
@@ -3,3 +3,18 @@ y=78
 z=x/y
 print(z)
 print('i  added a print statement ')
+
+def factorial_recursive(n):
+    if not isinstance(n, int):
+        raise ValueError("Input must be an integer.")
+    if n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n+1):
+            result *= i
+        return result
+    
+    
+
+factorial_recursive('3.0')
